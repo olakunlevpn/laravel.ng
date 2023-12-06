f56d481cfb7092bd23d8a0aa39f2036ac5cb862a
Olakunlevpn <olakunlevpn@live.com>
2023-12-06 09:28:25 -0800

    Added envanto license verification system

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('verify', function (){
-    $response = Http::get('http://laravel-ng.test/api/envato/verify/86781236-23d0-4b3c-7dfa-c1c147e0dece');
-
-    if ($response->successful()) {
-        $data = $response->json();
-        dump($data);
-    } else {
-        $error = $response->body();
-        dump($error);
-    }
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
