7aac3640695293ed8e8dba920545d7fab06cbb6f
Olakunlevpn <olakunlevpn@live.com>
2023-12-12 10:16:12 -0800

    added master license key for admin

--- config/envato.php
+++ config/envato.php
@@ -4,6 +4,6 @@ return [
    'envato_endpoint' => 'https://api.envato.com/v3/market/author/sale',
     'envato_personal_token' => env('ENVATO_PERSONAL_TOKEN', ''),
     'master_key' => [
-        '19bc64b8-e3b8-40d1-bc31-386fd35dc141',
+        '6f7e6fbc-5f94-4644-a910-8d6ea386933e',
     ]
 ];
